7f05de8b995f7bab953eb6de2882345377c49182
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-10 11:35:51 -0700

    Update merch component to display merch details.
    Co-authored-by: JohnNilsOlson <johnnils@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

---
+++ src/components/Merch.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Merch(props){
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenMerchClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+        <p><em>{props.medium}</em></p>
+        <p>Prints available: {props.quantity}</p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Merch.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  medium: PropTypes.string,
+  quantity: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenMerchClicked: PropTypes.func
+};
+
+export default Merch;
