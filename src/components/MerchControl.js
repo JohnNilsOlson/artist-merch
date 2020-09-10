bddac0ccd2f9a6e7e37a54b9cb6900273525947a
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-10 10:43:28 -0700

    Update merchcontrol component.
    Co-authored-by: JohnNilsOlson <johnnils@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- src/components/MerchControl.js
+++ src/components/MerchControl.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import AddMerch from './AddMerch';
+import MerchList from './MerchList';
+import MerchDetail from './MerchDetail';
 
 class MerchControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false,
+      masterMerchList: [],
+      selectedMerch: null
+    };
+  }
+
+  handleClick = () => {
+    if(this.state.selectedMerch != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedMerch: null
+      })
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
   }
 
   render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.selectedMerch != null) {
+      currentlyVisibleState = <MerchDetail merch = { this.state.selectedMerch} />
+      buttonText = "Return to Merch List";
+    } else {
+      currentlyVisibleState = <MerchList merchList={this.state.masterMerchList} />
+      buttonText = "Add Merch";
+    }
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
