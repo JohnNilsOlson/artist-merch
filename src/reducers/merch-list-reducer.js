a25d188ea55f858ea8bb8c375846e3dcbd044d9f
JohnNils Olson <johnnils@gmail.com>
2020-09-14 10:57:40 -0700

    add code to pass add merch test
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- src/reducers/merch-list-reducer.js
+++ src/reducers/merch-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, medium, quantity, id } = action;
+  switch (action.type) {
+    case "ADD_MERCH":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          medium: medium,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default: 
+      return state;
+  }
 };
