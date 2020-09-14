434c93a4cc489412279fc3bfcc498b5261c80ed1
JohnNils Olson <johnnils@gmail.com>
2020-09-14 14:40:12 -0700

    add root reducer smoke tests
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import merchListReducer from '../../reducers/merch-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -8,4 +13,34 @@ describe("rootReducer", () => {
       formVisibleOnPage: false
     });
   });
+
+  test('Check that intial state of merchListReducer matches root reducer', () => {
+    expect(store.getState().masterMerchList).toEqual(merchListReducer(undefined, { type: null}));
+  });
+
+  test('Check that intial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null}));
+  });
+
+  test('Check that intial state of merchListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_MERCH',
+      name: 'Test name',
+      description: 'Test.',
+      medium: 'Test',
+      quantity: 10,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterMerchList).toEqual(merchListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
+
