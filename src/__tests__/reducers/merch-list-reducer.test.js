07e11ad1d01a58b676a9554f5c8463f8569a69dd
JohnNils Olson <johnnils@gmail.com>
2020-09-14 10:52:01 -0700

    add failing test for add merch action
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- src/__tests__/reducers/merch-list-reducer.test.js
+++ src/__tests__/reducers/merch-list-reducer.test.js
@@ -1,7 +1,40 @@
 import merchListReducer from '../../reducers/merch-list-reducer';
 
 describe('merchListReducer', () => {
+
+  let action;
+  const merchData = {
+    name: "Test Name",
+    description: "Test description",
+    medium: "Test medium",
+    quantity: 10,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(merchListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new merch data to masterMerchList', () => {
+    const { name, description, medium, quantity, id } = merchData;
+    action = {
+      type: "ADD_MERCH",
+      name: name,
+      description: description,
+      medium: medium,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(merchListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        medium: medium,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
 });
