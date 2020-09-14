31d9067b98068296cf2e40a39fb437eff98af7b3
JohnNils Olson <johnnils@gmail.com>
2020-09-14 11:17:55 -0700

    add failing test for merch delete
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- src/__tests__/reducers/merch-list-reducer.test.js
+++ src/__tests__/reducers/merch-list-reducer.test.js
@@ -3,12 +3,17 @@ import merchListReducer from '../../reducers/merch-list-reducer';
 describe('merchListReducer', () => {
 
   let action;
-  const merchData = {
-    name: "Test Name",
+  const currentState = {
+    1: {name: "Test Name 1",
     description: "Test description",
     medium: "Test medium",
     quantity: 10,
-    id: 1
+    id: 1},
+    2: {name: "Test Name 2",
+    description: "Test description here",
+    medium: "Test medium - acrylic",
+    quantity: 60,
+    id: 2},
   };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
@@ -16,7 +21,7 @@ describe('merchListReducer', () => {
   });
 
   test('Should successfully add new merch data to masterMerchList', () => {
-    const { name, description, medium, quantity, id } = merchData;
+    const { name, description, medium, quantity, id } = currentState;
     action = {
       type: "ADD_MERCH",
       name: name,
@@ -37,4 +42,18 @@ describe('merchListReducer', () => {
     });
   });
 
+  test('Should successfully delete a merch item', () => {
+    action = {
+      type: 'DELETE_MERCH',
+      id: 1
+    };
+    expect(merchListReducer(currentState, action)).toEqual({
+      2: {name: "Test Name 2",
+      description: "Test description here",
+      medium: "Test medium - acrylic",
+      quantity: 60,
+      id: 2}
+    });
+  });
+  
 });
